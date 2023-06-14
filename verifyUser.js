9697460895b794253e714e8c585a40a097eefd1d
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-06-14 14:25:06 -0500

    Fixes

--- verifyUser.js
+++ verifyUser.js
@@ -1,19 +1,23 @@
 const axios = require("axios")
 
 async function verifyUser(request, response, next) {
-    
-    
+
+
     let auth = request.headers.authorization;
-    
+
     if (auth !== undefined) {
         let token = auth.split(" ")[1];
         let headers = {
             Authorization: `Bearer ${token}`
         }
-        let response = await axios.get(process.env.AUTH_DOMAIN, { headers: headers });
-        request.user = response.data;
-    }
-    next();
+        try {
+            let response = await axios.get(process.env.AUTH_DOMAIN, { headers: headers });
+        } catch (error) {
+            response.send(error);
+        }
+            request.user = response.data;
+        }
+        next();
 }
 
 module.exports = verifyUser;
